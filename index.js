5f60026db8458655e7374745ad1b44b8decf5c85
MafujulHaquePlabon <mafujul15-3076@diu.edu.bd>
2022-05-08 08:20:14 +0600

    server side initialization

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+const app =express();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qtxre.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+try{
+
+}
+finally{
+
+}
+}
+run().catch(console.dir)
+
+
+app.get('/',(req,res)=>{
+    res.send('warehouse is running');
+
+});
+
+app.listen(port,()=>{
+    console.log('warehouse is running',port);
+})
