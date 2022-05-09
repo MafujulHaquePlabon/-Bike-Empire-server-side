4b17f55f8b783afb35aec06c7d732b9c43eba0e3
MafujulHaquePlabon <mafujul15-3076@diu.edu.bd>
2022-05-09 21:55:19 +0600

    section added

--- index.js
+++ index.js
@@ -13,9 +13,31 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qtxre.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log('db connected')
 async function run(){
 try{
-
+    await client.connect();
+    const inventoryItemsCollection = client.db('warehouse').collection('inventoryItems');
+      // inventoryItems API
+      app.get('/inventoryItems', async (req, res) => {
+        const query = {};
+        const cursor =inventoryItemsCollection.find(query);
+        const inventoryItems = await cursor.toArray();
+        res.send(inventoryItems);
+    });
+    app.get('/ManageInventories', async (req, res) => {
+        const query = {};
+        const cursor =inventoryItemsCollection.find(query);
+        const ManageInventories = await cursor.toArray();
+        res.send(ManageInventories);
+    });
+    app.get('/ManageInventories/:id', async(req, res) =>{
+        const id = req.params.id;
+        const query = {_id:ObjectId(id)};
+        const result = await inventoryItemsCollection.findOne(query);
+        res.send(result);
+    });
+     
 }
 finally{
 
