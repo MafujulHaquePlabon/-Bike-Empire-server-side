2ce281b2d8f71c4fce6c4540406833d58f7c62c4
MafujulHaquePlabon <mafujul15-3076@diu.edu.bd>
2022-05-12 18:43:55 +0600

    Manage_Item route setup complete

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qtxre.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-console.log('db are connect')
+console.log('db are connected')
 async function run(){
 try{
     await client.connect();
@@ -31,6 +31,7 @@ try{
         const cursor =inventoryItemsCollection.find(query);
         const inventoryItems = await cursor.toArray();
         res.send(inventoryItems);
+      
     });
     app.get('/ManageInventories', async (req, res) => {
         const query = {};
@@ -38,7 +39,7 @@ try{
         const ManageInventories = await cursor.toArray();
         res.send(ManageInventories);
     });
-    app.get('/inventoryItems/:id', async(req, res) =>{
+    app.get('/inventoryItems/:id', async (req, res) =>{
         const id = req.params.id;
         const query = {_id:ObjectId(id)};
         const result = await inventoryItemsCollection.findOne(query);
