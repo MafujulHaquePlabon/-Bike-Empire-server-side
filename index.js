4fd47d0c4fa04abecc6fac7c3c3189c950adfb6a
MafujulHaquePlabon <mafujul15-3076@diu.edu.bd>
2022-05-12 12:36:57 +0600

    updated

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qtxre.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-console.log('db connected')
+console.log('db are connected')
 async function run(){
 try{
     await client.connect();
